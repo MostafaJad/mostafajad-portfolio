0c4190646ea7aa344784d92c7ba6e2044422d2dd
Jado <m.mohamed.jad@gmail.com>
2021-05-11 18:56:57 -0400

    NavBar and Hamburger menu

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import './portfolio.scss'
 export default function Portfolio() {
   return (
-    <div className="portfolio">
+    <div className="portfolio" id="portfolio">
       
     </div>
   )
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,7 +1,30 @@
 import './topbar.scss';
-export default function Topbar() {
+import {Person, Mail} from '@material-ui/icons'
+export default function Topbar({menuOpen, setMenuOpen}) {
   return (
-    <div className="topbar">
+    <div className={'topbar '+ (menuOpen && 'active')}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">Mostafa Jad</a>
+          <div className="itemContainer">
+            <Person className='icon' />
+            <span>+1 416 554 2532</span>
+          </div>
+          <div className="itemContainer">
+            <Mail classname='icon' />
+            <span>m.mohamed.jad@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={()=> setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+
+
+          </div>
+        </div>
+      </div>
       
     </div>
   )
